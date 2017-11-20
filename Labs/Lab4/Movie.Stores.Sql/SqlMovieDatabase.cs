2f950f70e37531867c0899c803f2ab5dd4eec991
ThomasWhiteTCCD <thomas.white139506@my.tccd.edu>
2017-11-20 13:37:38 -0600

    Lab 4 completed.

---
+++ Labs/Lab4/Movie.Stores.Sql/SqlMovieDatabase.cs
@@ -0,0 +1,150 @@
+﻿
+using MovieLib.Stores;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLib.Data.Sql
+{
+    /// <summary>Provides an implementation of <see cref="IMovieDatabase"/> using SQL Server.</summary>
+    public class SqlMovieDatabase : MovieDatabase 
+    {
+        #region Construction
+
+        public SqlMovieDatabase(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        private readonly string _connectionString;
+        #endregion
+
+        protected override Movie AddCore(Movie movie)
+        {
+            var id = 0;
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("AddMovie", conn) { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.Add("@title", SqlDbType.VarChar).Value = movie.Title;
+                cmd.Parameters.AddWithValue("@description", movie.Description);
+                cmd.Parameters.AddWithValue("@length", movie.Length);
+                cmd.Parameters.AddWithValue("@isOwned", movie.IsOwned);
+
+                id = Convert.ToInt32(cmd.ExecuteScalar());
+            };
+
+            return GetCore(id);
+        }
+
+        protected override IEnumerable<Movie> GetAllCore()
+        {
+            var movies = new List<Movie>();
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("GetAllMovies", conn) { CommandType = CommandType.StoredProcedure };
+                
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var movie = new Movie() {
+                            Id = reader.IsDBNull(0) ? 0 : reader.GetInt32(0),
+                            Title = reader.GetFieldValue<string>(1),
+                            IsOwned = reader.GetBoolean(4),
+                            Description = reader.GetString(2),
+                            Length = reader.GetInt32(3),             
+                        };
+                        movies.Add(movie);
+                    };
+                };
+
+                return movies;
+            };
+        }
+
+        protected override Movie GetCore(int id)
+        {
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("GetMovie", conn) { CommandType = CommandType.StoredProcedure };
+                cmd.Parameters.AddWithValue("@id", id);
+
+                //Using a dataset instead of a reader
+                var ds = new DataSet();
+                var da = new SqlDataAdapter() {
+                    SelectCommand = cmd,
+                };
+
+                da.Fill(ds);
+
+                var table = ds.Tables.OfType<DataTable>().FirstOrDefault();
+                if (table != null)
+                {
+                    var row = table.AsEnumerable().FirstOrDefault();
+                    if (row != null)
+                    {
+                        return new Movie() {
+                            Id = Convert.ToInt32(row["Id"]),
+                            Title = row.Field<string>("Title"),
+                            Description = row.Field<string>("Description"),
+                            Length = row.Field<int>("length"),
+                            IsOwned = row.Field<bool>("isowned")
+                        };
+                    };
+                };
+            };
+
+            return null;
+        }
+
+        protected override void RemoveCore(int id)
+        {
+            using (var conn = OpenDatabase())
+            {
+                //Alternative approach to creating command
+                var cmd = conn.CreateCommand();
+
+                cmd.CommandText = "RemoveMovie";
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                //Long way
+                var parameter = new SqlParameter("@id", id);
+                cmd.Parameters.Add(parameter);
+
+                cmd.ExecuteNonQuery();
+            };
+        }
+
+        protected override Movie UpdateCore(Movie existing, Movie movie)
+        {
+            using (var conn = OpenDatabase())
+            {
+                var cmd = new SqlCommand("UpdateMovie", conn) { CommandType = CommandType.StoredProcedure };
+
+                cmd.Parameters.AddWithValue("@id", existing.Id);
+                cmd.Parameters.AddWithValue("@title", movie.Title);
+                cmd.Parameters.AddWithValue("@description", movie.Description);
+                cmd.Parameters.AddWithValue("@length", movie.Length);
+                cmd.Parameters.AddWithValue("@isOwned", movie.IsOwned);
+
+                cmd.ExecuteNonQuery();
+            };
+
+            return GetCore(existing.Id);
+        }
+
+        private SqlConnection OpenDatabase()
+        {
+            var connection = new SqlConnection(_connectionString);
+
+            connection.Open();
+
+            return connection;
+        }
+    }
+}
---
+++ Labs/Lab4/MovieLib/Movie.cs
@@ -0,0 +1,59 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLib 
+{
+    /// <summary>Represents a movie.</summary>
+    /// <remarks>This will represent a movie with other stuff.</remarks>
+    public class Movie : IValidatableObject
+    {
+        ///<summary>Gets or sets the unique identifier.</summary>
+        public int Id { get; set; }
+
+        /// <summary>Gets or sets the title.</summary>
+        /// <value>Never returns null.</value>
+        public string Title
+        {
+            get => _title ?? ""; 
+            set => _title = value?.Trim();
+        }
+
+        /// <summary>Gets or sets the description.</summary>
+        public string Description
+        {
+            get => _description ?? ""; 
+            set => _description = value?.Trim(); 
+        }
+
+        /// <summary>Gets or sets the length.</summary>
+        public int Length { get; set; } = 0;
+
+        /// <summary>Determines if owned.</summary>
+        public bool IsOwned { get; set; }
+
+        public override string ToString() => Title;
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            //Title cannot be empty
+            if (String.IsNullOrEmpty(Title))
+                yield return new ValidationResult("Title cannot be empty.", new[] { nameof(Title) });
+
+            //Length >= 0
+            if (Length < 0)
+                yield return new ValidationResult("Length must be >= 0.", new[] { nameof(Length) });
+        }
+
+        #region Private Members
+
+        private string _title;
+        private string _description;
+        #endregion
+    }  
+}
